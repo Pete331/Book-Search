3b3721656e46f9ee601cae0652a435a32cf4bd90
Pete Brennan <pweda64@hotmail.com>
2020-07-18 07:23:09 +0800

    updated readme

--- client/src/components/BookResults.js
+++ client/src/components/BookResults.js
@@ -1,14 +1,9 @@
 import React from "react";
 import BookListItem from "./BookListItem";
 
-function BookResults({
-  books,
-  saved,
-  handleSaveClick,
-  handleDeleteClick,
-}) {
+function BookResults({ books, saved, handleSaveClick, handleDeleteClick }) {
   return (
-    <div className="card-panel" style={{backgroundColor: "blanchedalmond"}}>
+    <div className="card-panel" style={{ backgroundColor: "blanchedalmond" }}>
       <p>Results</p>
 
       {books.length ? (
@@ -19,7 +14,11 @@ function BookResults({
               authors={book.volumeInfo.authors}
               link={book.volumeInfo.infoLink}
               description={book.volumeInfo.description}
-              image={book.volumeInfo.imageLinks.thumbnail}
+              image={
+                book.volumeInfo.imageLinks == undefined
+                  ? "https://via.placeholder.com/150"
+                  : book.volumeInfo.imageLinks.thumbnail
+              }
               key={book.id}
               id={book.id}
               handleSaveClick={handleSaveClick}
